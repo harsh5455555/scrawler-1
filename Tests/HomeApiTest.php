24f5fb5ece9f7835fca5982d584032b9292cc937
Pranjal Pandey <pranjaltech10@gmail.com>
2016-04-11 21:20:50 +0530

    stepping to future

--- Tests/HomeApiTest.php
+++ Tests/HomeApiTest.php
@@ -16,13 +16,12 @@ class HomeApiTest extends \PHPUnit_Framework_TestCase {
     function testGET(){
     $client = new \GuzzleHttp\Client(['base_uri' => 'http://localhost:8000']);
      
-     $request=$client->request('GET', '/', [
+     $response=$client->request('GET', '/', [
     'headers' => [
         'X-SCRAWLER-USERNAME'      => "scrawler",
         'X-SCRAWLER-PASSWORD'      => "xjb48b43e2z"         ]
 ]);
 
-    $response = $request->send();
     $this->assertEquals(201, $response->getStatusCode());
     $data = json_decode($response->getBody(true), true);
     $this->assertArrayHasKey('test', $data);
