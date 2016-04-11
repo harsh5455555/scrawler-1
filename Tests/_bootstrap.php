863d07168ae9219d21df90c2cdceed3255a0cc4d
Pranjal Pandey <pranjaltech10@gmail.com>
2016-04-11 20:10:13 +0530

    fixing guzzle

--- Tests/_bootstrap.php
+++ Tests/_bootstrap.php
@@ -2,3 +2,4 @@
 require_once __DIR__."/../Core/Start.php";
 loader()->registerNamespace('Tests', __DIR__.'/../');
 loader()->registerNamespace('GuzzleHttp',__DIR__.'/');
+include __DIR__ . '/GuzzleHttp/functions_include.php';
